344d8cf5821e88b0fe0d9d0aea5b252351ad52d1
urimJ <ufrl128@dgu.ac.kr>
2024-07-29 20:42:55 +0900

    MOD sequelize와 데이터베이스 연결

--- DB.js
+++ DB.js
@@ -11,3 +11,10 @@ const sequelize = new Sequelize(
         dialect: mysql
     }
 );
+
+try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+} catch(error){
+    console.error('Unable to connect to the database:', error);
+}
